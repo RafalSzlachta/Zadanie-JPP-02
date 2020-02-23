06ba9819754e6b74af84e9769067f3e914edadf8
unknown <uzytkownik@o2.pl>
2020-02-23 10:32:59 +0100

    write code

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -1,7 +1,30 @@
 package pl.sda;
 
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+        int[] tab = {25, 64, -52, 125, -38};
+        System.out.println("Największa liczba w tablicy to: " + maximum(tab));
+        System.out.println("Najmniejsza liczba w tablicy to: " + minimum(tab));
 
+}
+
+    private static int minimum(int[] tab) {
+        int maxDigit = tab[0];
+        for (int i = 0; i < tab.length; i++){
+            if (maxDigit>tab[i]){
+                maxDigit = tab[i];
+            }
+        }
+        return maxDigit;
+    }
+
+    private static int maximum(int[] tab) {
+        int maxDigit = tab[0];
+        for (int i = 0; i < tab.length; i++){
+            if (maxDigit<tab[i]){
+                maxDigit = tab[i];
+            }
+        }
+        return maxDigit;
     }
 }
